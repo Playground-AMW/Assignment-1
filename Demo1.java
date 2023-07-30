62816ff89174356d976d1ea0b219029f9aa0ba95
Achinda <achindamihiruk@gmail.com>
2023-07-30 08:32:48 +0530

    Completed Task 1

---
+++ Demo1.java
@@ -0,0 +1,7 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        float bankAmt = 123456.7890f;
+
+        System.out.printf("Your bank amount is: $%,.2f\n", bankAmt);
+    }
+}
